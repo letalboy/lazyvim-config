8dc723998c67ee5702dac78633b173007c375c3f
Cristian Camargo <ccf@cdone.com.br>
2024-01-26 22:25:56 -0300

    Add more keymasp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,11 +10,56 @@ vim.keymap.set("n", "<Leader>O", "O<Esc>^Da", vim.opts)
 vim.keymap.set("n", "te", ":tabedit")
 vim.keymap.set("n", "<tab>", ":tabnext<Return>", vim.opts)
 vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", vim.opts)
+
 -- Split window
 vim.keymap.set("n", "ss", ":split<Return>", vim.opts)
 vim.keymap.set("n", "sv", ":vsplit<Return>", vim.opts)
+
 -- Move window
 vim.keymap.set("n", "sh", "<C-w>h")
 vim.keymap.set("n", "sk", "<C-w>k")
 vim.keymap.set("n", "sj", "<C-w>j")
 vim.keymap.set("n", "sl", "<C-w>l")
+
+-- Move lines with alt + arrow up or arrow down
+vim.api.nvim_set_keymap("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+-- Cut lines with ctrl + x
+vim.api.nvim_set_keymap("n", "<C-X>", "dd", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-X>", "d", { noremap = true, silent = true })
+
+-- Fast copy
+vim.api.nvim_set_keymap("n", "<C-C>", "yy", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-C>", "y", { noremap = true, silent = true })
+
+-- Fast paste
+vim.api.nvim_set_keymap("n", "<C-V>", "p", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-V>", '<C-R>"', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-V>", "p", { noremap = true, silent = true })
+
+-- Change window
+for i = 1, 9 do
+  vim.api.nvim_set_keymap("n", "<C-" .. i .. ">", i .. "gt", { noremap = true, silent = true })
+end
+
+-- Undo
+vim.api.nvim_set_keymap("n", "<C-Z>", "u", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-Z>", "<C-O>u", { noremap = true, silent = true })
+
+-- Redo
+vim.api.nvim_set_keymap("n", "<C-S-Z>", "<C-R>", { noremap = true, silent = true })
+
+-- Create a line att top:
+vim.api.nvim_set_keymap("n", "<C-S-M>", ":execute 'normal! gg' | startinsert<CR>", { noremap = true, silent = true })
+
+-- Create a line att bottom
+vim.api.nvim_set_keymap("n", "<C-S-N>", ":execute 'normal! G' | startinsert<CR>", { noremap = true, silent = true })
+
+-- Tab to left using shit + tab
+vim.api.nvim_set_keymap("n", "<C-D>", "<<", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-D>", "<", { noremap = true, silent = true })
