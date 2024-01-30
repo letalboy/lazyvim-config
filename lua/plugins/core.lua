47f33f35933330a949b085efba22100bd09e9758
Cristian Camargo <ccf@cdone.com.br>
2024-01-30 14:35:19 -0300

    Sync

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -25,4 +25,14 @@ return {
       })
     end,
   },
+  {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
 }
