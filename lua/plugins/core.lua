9f3626be8a14087e1697d62a665fd35ac6241f50
Cristian Camargo <ccf@cdone.com.br>
2024-02-16 23:30:37 -0300

    Add astro framework plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -68,4 +68,5 @@ return {
     },
     lazy = false,
   },
+  { "wuelnerdotexe/vim-astro" },
 }
--- lv-config.lua
+++ lv-config.lua
@@ -8,3 +8,4 @@ vim.lang.svelte.lsp.setup = {
 vim.use("evanleck/vim-svelte")
 vim.use("iamcco/markdown-preview.nvim")
 vim.use("numToStr/Comment.nvim")
+vim.use("wuelnerdotexe/vim-astro")
