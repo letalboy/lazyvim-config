fef97c7311241bd43dfc61042f100952c7eaab09
Cristian Camargo <ccf@cdone.com.br>
2024-02-21 03:55:08 -0300

    Solve the astro autocomplete, the issues was that tailwind formater was taking atros and making other lsp don't be able to attach to the file

--- init.lua
+++ init.lua
@@ -24,17 +24,13 @@ cmp.setup({
   }),
 })
 
--- Setup lspconfig.
 local lspconfig = require("lspconfig")
-
--- Common on_attach function to run for every LSP server.
-local on_attach = function(client, bufnr)
-  -- Customize LSP key mappings here
-end
 
 -- Setup LSP servers.
 lspconfig.tsserver.setup({
-  on_attach = on_attach,
+  on_attach = function(client, bufnr)
+    -- Custom logic to enhance the server's capabilities
+  end,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
   -- Other tsserver specific settings can be added here
 })
@@ -48,6 +44,64 @@ lspconfig.lua_ls.setup({
         globals = { "vim" },
       },
     },
+  },
+})
+
+-- Disable tailwindcss language server for .astro files
+require("lspconfig").tailwindcss.setup({
+  on_attach = function(client, bufnr)
+    if vim.bo[bufnr].filetype == "astro" then
+      client.server_capabilities.document_formatting = false
+      client.server_capabilities.document_range_formatting = false
+    end
+    -- Your on_attach function body
+  end,
+  filetypes = { "html", "css", "postcss" }, -- Remove "astro" from this list
+})
+
+require("lspconfig").astro.setup({
+  on_attach = function(client, bufnr)
+    -- Custom logic for Astro files
+  end,
+  filetypes = { "astro" },
+  -- Other configurations...
+})
+
+-- Setup for TypeScript and JavaScript files
+require("lspconfig").tsserver.setup({
+  on_attach = function(client, bufnr)
+    -- Your on_attach function body
+  end,
+  filetypes = {
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+  },
+  -- Other tsserver configurations
+})
+
+-- Assuming you have null-ls and Prettier installed and configured globally
+require("null-ls").setup({
+  sources = {
+    require("null-ls").builtins.formatting.prettier.with({
+      filetypes = { "astro", "javascript", "typescript" },
+    }),
+  },
+})
+
+-- Use 'null-ls' for linting and formatting support
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettier.with({
+      extra_filetypes = { "astro" },
+    }),
+    null_ls.builtins.diagnostics.eslint.with({
+      extra_filetypes = { "astro" },
+    }),
   },
 })
 
--- lv-config.lua
+++ lv-config.lua
@@ -9,9 +9,3 @@ vim.use("evanleck/vim-svelte")
 vim.use("iamcco/markdown-preview.nvim")
 vim.use("numToStr/Comment.nvim")
 vim.use("wuelnerdotexe/vim-astro")
-
--- vim.filetype.add({
---   extension = {
---     astro = "astro",
---   },
--- })
